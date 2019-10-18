56161d564575194710cbcee4f5dc756b68468e46
dilansql <dilan.ormisiar@gilmond.com>
2019-10-19 00:05:32 +0100

    addition

---
+++ Database1/Scripts/AzureDBMagicTheGatheringSchemaScript.sql
@@ -0,0 +1,44 @@
+﻿--MagicTheGathering Mana Tables
+	create schema magicthegathering;
+	
+		go
+	
+		if object_id ('magicthegathering.manastate') is not null drop table magicthegathering.manastate;
+
+			create table magicthegathering.manastate 
+				(
+					mana_colour varchar (10) not null references magicthegathering(manacolour),
+					mana_value int not null,
+				);
+	
+		if object_id ('magicthegathering.manastateaudit') is not null drop table magicthegathering.manastateaudit;
+
+			create table magicthegathering.manastateaudit 
+				(
+					mana_colour_id varchar (10) not null references magicthegathering(manacolour),
+					mana_value_addition int not null,
+					mana_datetime datetime not null unique
+				);
+	
+		if object_id ('magicthegathering.manacolour') is not null drop table magicthegathering.manacolour;	
+
+			create table magicthegathering.manacolour
+				(
+					mana_colour_id int not null primary key identity, 
+					mana_colour varchar (10) not null unique
+				);
+	
+					insert into magicthegathering.manacolour values ( 1, 'Colourless' );
+													
+					insert into magicthegathering.manacolour values ( 2, 'Blue' );
+													
+					insert into magicthegathering.manacolour values ( 3, 'Red' );
+													
+					insert into magicthegathering.manacolour values ( 4, 'Green' );
+												
+					insert into magicthegathering.manacolour values ( 5, 'White' );
+													
+					insert into magicthegathering.manacolour values ( 6, 'Black' );		
+
+		if object_id ('magicthegathering.manacolour') is not null drop table magicthegathering.manacolour;
+		--Create a View
